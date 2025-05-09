18d499740b7b3b198877d457c4a1d51505477089
Daniel1205375 <l21200211@pachuca.tecnm.mx>
2025-05-08 20:21:56 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // Función para obtener los datos de la API y actualizar la tabla
 function fetchData() {
-    fetch('http://52.3.226.240:80/api/devices')
+    fetch('http://98.81.55.37:80/api/devices')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Error en la respuesta de la API');
